b3699958942d5b0f087c11765c53e1e1f9251512
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-02-27 01:25:22 +0300

    Add DI

--- Patients/Program.cs
+++ Patients/Program.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using Patients.Data.Data.Entities;
 
 namespace Patients
 {
   internal static class Program
   {
-    /// <summary>
-    /// Главная точка входа для приложения.
-    /// </summary>
+    public static IServiceProvider ServiceProvider { get; set; }
+
+    static void ConfigureServices()
+    {
+      var services = new ServiceCollection();
+      services.AddTransient<Patient>();
+
+      ServiceProvider = services.BuildServiceProvider();
+    }
+
     [STAThread]
     private static void Main()
     {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
+      ConfigureServices();
       Application.Run(new MainForm());
     }
   }
