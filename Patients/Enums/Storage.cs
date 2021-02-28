9dc93b4a13db1e8bb8d58543fe993bd8b13492de
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-02-28 23:35:24 +0300

    Add Descriptions to enums

--- Patients/Enums/Storage.cs
+++ Patients/Enums/Storage.cs
@@ -1,11 +1,18 @@
-﻿namespace Patients.Enums
+﻿using System.ComponentModel;
+
+namespace Patients.Enums
 {
   public enum Storage
   {
+    [Description("Бумажный носитель")]
     Paper,
+    [Description("Пациент")]
     Patient,
+    [Description("Компьютер (KartaWpf)")]
     KartaWpf,
+    [Description("Компьютер (Dental)")]
     Dental,
+    [Description("Компьютер")]
     Computer
   }
 }
--- Patients/Enums/ToothStatus.cs
+++ Patients/Enums/ToothStatus.cs
@@ -1,13 +1,22 @@
-﻿namespace Patients.Enums
+﻿using System.ComponentModel;
+
+namespace Patients.Enums
 {
   public enum ToothStatus
   {
+    [Description("Здоровый зуб")]
     Healthy,
+    [Description("Кариес, осложнения кариеса")]
     Caries,
+    [Description("Пломба")]
     Seal,
+    [Description("Зуб отсутствует")]
     Removed,
+    [Description("Корень")]
     Root,
+    [Description("Искусственная коронка")]
     ArtificialCrown,
+    [Description("Искусственный зуб")]
     Artificial
   }
 }
