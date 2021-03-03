49bf3716eb9e8c43f6349c269b27d23fc45b16e0
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-03-03 14:14:00 +0300

    Rename Payment's DateTime column to Date

--- Patients/Data/Entities/Payment.cs
+++ Patients/Data/Entities/Payment.cs
@@ -6,7 +6,7 @@ namespace Patients.Data.Entities
   {
     public Guid ID { get; set; }
     public decimal Amount { get; set; }
-    public DateTime DateTime { get; set; }
+    public DateTime Date { get; set; }
 
     public virtual Patient Patient { get; set; }
     public Guid PatientID { get; set; }
