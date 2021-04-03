7d95280ae547159f7df0a75d7978f06253adf785
Dmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-04-03 13:27:42 +0300

    Add new column to Payments table - Diagnosis

---
+++ Patients/Data/Migrations/20210403102517_AddDiagnosisColumnMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Patients.Migrations
+{
+  public partial class AddDiagnosisColumnMigration: Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.AddColumn<string>(
+        name: "Diagnosis",
+        table: "Payments",
+        type: "TEXT",
+        nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropColumn(
+        name: "Diagnosis",
+        table: "Payments");
+    }
+  }
+}
