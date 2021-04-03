6e8e2908841b27ccb9025bf49806bab1ac0eecee
Dmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-04-03 18:09:57 +0300

    Create PatientTeeth and Teeth repos and add them to UOF

---
+++ Patients/Data/Repositories/PatientTeethRepository.cs
@@ -0,0 +1,12 @@
+﻿using Patients.Data.Entities;
+using Patients.Data.Repositories.Interfaces;
+
+namespace Patients.Data.Repositories
+{
+  internal class PatientTeethRepository: RepositoryBase<PatientTooth>, IPatientTeethRepository
+  {
+    public PatientTeethRepository(AppDbContext appDbContext)
+      : base(appDbContext)
+    { }
+  }
+}
--- Patients/Data/UnitOfWork.cs
+++ Patients/Data/UnitOfWork.cs
@@ -13,17 +13,21 @@ namespace Patients.Data
     private IDiaryRecordsRepository _diaryRecordsRepository;
     private IDentalRecordsRepository _dentalRecordsRepository;
     private IPaymentsRepository _paymentsRepository;
-    private bool _disposedValue = false;
+    private ITeethRepository _teethRepository;
+    private IPatientTeethRepository _patientTeethRepository;
+    private bool _disposed = false;
 
     public UnitOfWork(AppDbContext context)
     {
       _context = context;
     }
 
-    public IPatientsRepository Patients => _patientsRepository = _patientsRepository ?? new PatientsRepository(_context);
-    public IDiaryRecordsRepository DiaryRecords => _diaryRecordsRepository = _diaryRecordsRepository ?? new DiaryRecordsRepository(_context);
-    public IDentalRecordsRepository DentalRecords => _dentalRecordsRepository = _dentalRecordsRepository ?? new DentalRecordsRepository(_context);
-    public IPaymentsRepository Payments => _paymentsRepository = _paymentsRepository ?? new PaymentsRepository(_context);
+    public IPatientsRepository Patients => _patientsRepository ??= new PatientsRepository(_context);
+    public IDiaryRecordsRepository DiaryRecords => _diaryRecordsRepository ??= new DiaryRecordsRepository(_context);
+    public IDentalRecordsRepository DentalRecords => _dentalRecordsRepository ??= new DentalRecordsRepository(_context);
+    public IPaymentsRepository Payments => _paymentsRepository ??= new PaymentsRepository(_context);
+    public ITeethRepository Teeth => _teethRepository ??= new TeethRepository(_context);
+    public IPatientTeethRepository PatientTeeth => _patientTeethRepository ??= new PatientTeethRepository(_context);
 
     public async Task<bool> SaveAsync()
     {
@@ -46,14 +50,14 @@ namespace Patients.Data
 
     protected virtual void Dispose(bool disposing)
     {
-      if (!_disposedValue)
+      if (!_disposed)
       {
         if (disposing)
         {
           _context.Dispose();
         }
 
-        _disposedValue = true;
+        _disposed = true;
       }
     }
 
